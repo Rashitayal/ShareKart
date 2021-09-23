1db7dabdabc40bdec6a44211b5823f428d2ecf36
Sushil Ranjan <suranjan@informatica.com>
2021-09-23 22:14:02 +0530

    CrossOrigin bypass

--- src/main/java/com/informatica/hackathon/ShareKart/controller/RecommendationController.java
+++ src/main/java/com/informatica/hackathon/ShareKart/controller/RecommendationController.java
@@ -1,10 +1,31 @@
 package com.informatica.hackathon.ShareKart.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.informatica.hackathon.ShareKart.exception.InputValidator;
+import com.informatica.hackathon.ShareKart.exception.InvalidRequestException;
+import com.informatica.hackathon.ShareKart.model.RecommendationResponse;
+import com.informatica.hackathon.ShareKart.service.impl.AllProductRecommendation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/v1/recommend")
+@RequestMapping("/api/v1/profile")
 public class RecommendationController {
+
+    @Autowired
+    private AllProductRecommendation allProductRecommendation;
+
+    @RequestMapping(method = RequestMethod.GET,
+            consumes = "application/json", produces = "application/json")
+    public ResponseEntity<RecommendationResponse> getRecommendation(@PathVariable(value = "searchString") String searchString) throws InvalidRequestException {
+        InputValidator.validateSearch(searchString);
+        //TODO : why its returning integer list in UI need data only we cnt show Ids.
+        List<Integer> integerList = allProductRecommendation.searchLikedProductIds(searchString);
+        return new ResponseEntity<RecommendationResponse>(, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/informatica/hackathon/ShareKart/exception/InputValidator.java
+++ src/main/java/com/informatica/hackathon/ShareKart/exception/InputValidator.java
@@ -1,10 +1,15 @@
 package com.informatica.hackathon.ShareKart.exception;
 
-public class EmailValidator {
+public class InputValidator {
 
     public static void validateEmail(String email) throws InvalidRequestException {
         if (!org.apache.commons.validator.routines.EmailValidator.getInstance().isValid(email)) {
             throw new InvalidRequestException("Invalid Email with value : " + email);
+        }
+    }
+    public static void validateSearch(String search) throws InvalidRequestException {
+        if (search.isEmpty()) {
+            throw new InvalidRequestException("Invalid search : " + search);
         }
     }
 }
